b58d09ff11d2d3b8e9b135d0ce6e92ed173fe8aa
CrypticSquirrel <brejae@andrews.edu>
2020-05-19 14:20:19 -0400

    Added POST requests to server
    - server: added cors middleware (allows our app to communicate)
    - server: send back just the token (might change to add username)
    - client: added login/signup http post requests w/ user info

--- client/scripts/login.js
+++ client/scripts/login.js
@@ -1,20 +1,50 @@
+const LOGIN_URL = 'http://localhost:3000/auth/login';
 
-$(document).ready(function() {
-	$('#login').click(function () {
-        getloginvalues();
+$(document).ready(function () {
+    $('#login').click(function (event) {
+        event.preventDefault();
+        login();
     });
-}); 
+});
 
-/* -------------------------------------- Element Selectors ------------------------------------- */
+/* ---------------------------------------- Handles Login --------------------------------------- */
 
-function getloginvalues() {
-	var username = $("#username").val();
-	var password = $("#pwd").val();
-	
-	
-	//Check:
-	alert(
-	"username: " + username + "  password: " + password
-	); 
+function login() {
+    var username = $('#username').val();
+    var password = $('#pwd').val();
+
+    const body = {
+        username,
+        password,
+    };
+
+	/**
+	 * Makes a POST request to server @localhost:3000/auth/login with login info. 
+	 * Returns a token to store in local memory. This will be their identifier in our site.
+	 * @todo redirect user when successful login
+	 */
+    fetch(LOGIN_URL, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+            'content-type': 'application/json',
+        },
+    })
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            }
+            return response.json().then((error) => {
+                throw new Error(error.message);
+            });
+        })
+        .then((result) => {
+            console.log(`token: ${result.token}`);
+            localStorage.token = result.token;
+            // window.location.href = "url/to/homepage";
+        })
+        .catch((error) => {
+            console.log(`error: ${error}`);
+            alert(error.message);
+        });
 }
-
--- client/scripts/signup.js
+++ client/scripts/signup.js
@@ -1,17 +1,52 @@
+const SIGNUP_URL = 'http://localhost:3000/auth/signup';
 
 $(document).ready(function () {
-    $('#signup').click(function () {
-        getsignupvalues();
+    $('#signup').click(function (event) {
+        event.preventDefault();
+        signup();
     });
 });
 
-/* -------------------------------------- Element Selectors ------------------------------------- */
+/* --------------------------------------- Handles Signup --------------------------------------- */
 
-function getsignupvalues() {
-    var email = $('#email').val();
-    var password = $('#pwd').val();
-    var confirmpwd = $('#cpwd').val();
+function signup() {
+    const email = $('#email').val();
+    const password = $('#pwd').val();
+    const confirmpwd = $('#cpwd').val();
 
-    //Check:
-    alert('email: ' + email + '  password: ' + password);
+    const body = {
+        username: email,
+        password,
+        repeat_password: confirmpwd,
+    };
+
+    /**
+	 * Makes a POST request to server @localhost:3000/auth/signup with signup info. 
+	 * Redirects user to login page when signup is successful. Alert if error.
+	 */
+    fetch(SIGNUP_URL, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+            'content-type': 'application/json',
+        },
+    })
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            }
+            return response.json().then((error) => {
+                throw new Error(error.message);
+            });
+        })
+        .then((result) => {
+            window.location.href = '../index.html';
+            console.log(`token: ${result.token}`);
+            localStorage.token = result.token;
+        })
+        .catch((error) => {
+            this.errorMessage = error.message;
+            console.log(`error: ${error}`);
+            alert(error.message);
+        });
 }
